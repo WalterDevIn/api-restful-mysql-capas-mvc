47e8c79f247e90a206c3eef26517926f652c4389
godOmager <callizayazenteno.wendy@tecnica7.edu.ar>
2025-10-15 20:25:48 -0300

    todo lo hizo julian

--- src/controllers/users.controllers.js
+++ src/controllers/users.controllers.js
@@ -1,72 +1,55 @@
 import pool from '../../config/conexion.js'
 //ASYNC es una promesa -> tiene que esperar a que le lleguen los datos para continuar la ejecucion
+import * as model from '../models/users.model.js'
 
-//mostrar todos los usuarios
 export const getAllUser = async (req, res) => {
-    const sql = "SELECT * FROM users"; //consulta SQL
-  
-    try{ //intenta hacer la promesa
-      const conection = await pool.getConnection(); //activa la conexion
-      const [rows] = await conection.query(sql); //ejecuta la consulta -> el SQL
-      conection.release(); //libera la conexion
-      res.json(rows); //envia la respuesta
-    }catch(error){ //si hay un error
-      res.status(500).send("algo salio mal")
-    }
-  
+  const rows= await model.getAllUser()
+  if(rows.error){
+     return res.status(500).send("algo salio mal")
+  }
+  (rows.length >0)? res.json(rows): res.send("NO HAY USUARIOSSSSSSSS")
 }
+
+
+
 //mostrar usuarios por id
 export const getUserById = async (req, res) => {
     const id = req.params.id;
-    const sql = `SELECT * FROM users WHERE id_user = ?`; //SQL q quiero ejecutar -> trae todo los q tiene la tabla "tienda"
-  
-    try{ 
-      const conection = await pool.getConnection(); 
-      const [rows] = await conection.query(sql, [id]);
-      conection.release(); //libera la conexion
-      
-      (rows[0])? res.json(rows[0]) : res.status(404).send("usuario no existe")
-      
-  
-    }catch(error){ //si hay un error
-      res.status(500).send("algo salio mal")
-    }
+    const rows = await model.getUserById(id)
+    //si viene del catch
+    if(rows.error){
+     return res.status(500).send("algo salio mal")
+  }
+
+  //si viene del try
+    (rows[0])? res.json(rows[0]) : res.status(404).send("usuario no existe")
 }
 //insertar usuarios
 export const createUser = async (req, res) => {
     const values = req.body
-    const sql = `INSERT INTO users SET ?`
-  
-    try{ 
-      const conection = await pool.getConnection(); 
-      const [rows] = await conection.query(sql, [values]);
-      conection.release(); 
-      
-      console.log(rows)
-      res.status(201).send(`nuevo usuario con id ${rows.insertId}`)
-      
-  
-    }catch(error){ 
-      res.status(500).send("algo salio mal")
+    const rows = model.createUser(values)
+    //catch
+    if(rows.error){
+     return res.status(500).send("algo salio mal")
     }
+  //try
+  //res.json(rows)
+    res.status(201).send(`nuevo usuario con id ${rows.insertId}`)
+    
 }
 //actializar datos -> cambiar datos de usuarios existentes
 export const updateUser = async(req, res) => {
-    const id = req.params.id;
-    const newValues = req.body
-    const sql = `UPDATE users SET ? WHERE id_user = ?`;
-  
-    try{ 
-      const conection = await pool.getConnection(); 
-      const [rows] = await conection.query(sql, [newValues, id]);
-      conection.release(); 
-      
-      console.log(rows);
-      (rows.affectedRows == 0)? res.status(404).send("Usuario no existe") : res.send("Datos actualizados")
-  
-    }catch(error){ 
-      res.status(500).send("algo salio mal")
-    }
+     const id = req.params.id;
+     const newValues = req.body
+     const rows = model.updateUser(newValues, id)
+      //catch
+      if(rows.error){
+         return res.status(500).send("algo salio mal")
+      }
+
+    //try
+   (rows.affectedRows == 0)? res.status(404).send("Usuario no existe") : res.send("Datos actualizados")
+
 }
 //eliminar usuarios por id
 export const deleteUser = async(req, res) => {
